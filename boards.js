04bbe3da7b9a8b08cbce447981d4bd7a0f79f64d
rpmd69 <rpmd69@durham.ac.uk>
2019-01-15 22:16:18 +0000

    Added event listener's and a working form

--- boards.js
+++ boards.js
@@ -1,7 +1,7 @@
 class Boards {
 	constructor(xPosition, yPosition, rectX, rectY, depth, colour) {
-		this.width = xPosition * 2 || 640;
-		this.height = yPosition * 2 || 480;
+		this.xPosition = xPosition * 2 || 640;
+		this.yPosition = yPosition * 2 || 480;
 		this.rectX = rectX || 100;
 		this.rectY = rectY || 200;
 		strokeWeight(depth || 4);
@@ -10,15 +10,39 @@ class Boards {
 		stroke(255, 251, 249);
 	}
 	
+	setXPosition(xPosition) {
+		this.xPosition = xPosition;
+	}
+	
+	setYPosition(yPosition) {
+		this.yPosition = yPosition;
+	}
+	
+	setRectX(rectX) {
+		this.rectX = rectX;
+	}
+	
+	setRectY(rectY) {
+		this.rectY = rectY;
+	}
+	
+	setDepth(depth) {
+		this.depth = depth;
+	}
+	
+	setColour(colour) {
+		this.colour = colour;
+	}
+	
 	draw() {
-		translate(this.width / 2, this.height / 2);
+		translate(this.xPosition / 2, this.yPosition / 2);
 		var num = 10;
-		var intervalX = map(mouseX, 0, this.width, 40, -40);
-		var intervalY = map(mouseY, 0, this.width, 20, -40);
-		var tilt = 20 * sin(2 * atan((this.height - 2 * mouseY) / (this.width - 2 * mouseX)));
+		var intervalX = map(mouseX, 0, this.xPosition, 40, -40);
+		var intervalY = map(mouseY, 0, this.xPosition, 20, -40);
+		var tilt = 20 * sin(2 * atan((this.yPosition - 2 * mouseY) / (this.xPosition - 2 * mouseX)));
 		for (var i = num - 1; i > 0; i--) {
 			push();
-			var rhytm = map(pow(abs(sin(frameCount * 0.03 - i * 0.3)), 50), 0, 1, 0, -50) * map(abs(mouseX - this.width / 2), 0, this.width / 2, 0, 1);
+			var rhytm = map(pow(abs(sin(frameCount * 0.03 - i * 0.3)), 50), 0, 1, 0, -50) * map(abs(mouseX - this.xPosition / 2), 0, this.xPosition / 2, 0, 1);
 			translate(intervalX * (i - num / 2.0), intervalY * (i - num / 2.0) + rhytm);
 			beginShape();
 			vertex(-this.rectX / 2.0, -this.rectY / 2.0 + tilt);
--- index.js
+++ index.js
@@ -9,3 +9,60 @@ function draw() {
 	background(255, 251, 249);
 	b.draw();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+	
+    function changeXPosition(event) {
+		let xPosition = document.getElementById("xPosition").value;
+		b.setXPosition(xPosition);
+    }
+	
+	function changeYPosition(event) {
+		let yPosition = document.getElementById("yPosition").value;
+		b.setYPosition(yPosition);
+    }
+	
+	function changeRectX(event) {
+		let rectX = document.getElementById("rectX").value;
+		b.setRectX(rectX);
+    }
+	
+	function changeRectY(event) {
+		let rectY = document.getElementById("rectY").value;
+		b.setRectY(rectY);
+    }
+	
+	function changeDepth(event) {
+		let depth = document.getElementById("depth").value;
+		b.setDepth(depth);
+    }
+	
+	function changeColour(event) {
+		let colour = document.getElementById("colour").value;
+		b.setColour(colour);
+	}
+	
+	var xp = document.getElementById("xPosition");
+	xp.addEventListener("input", changeXPosition);
+	
+	var yp = document.getElementById("yPosition");
+	yp.addEventListener("input", changeYPosition);
+	
+	var rx = document.getElementById("rectX");
+	rx.addEventListener("input", changeRectX);
+	
+	var ry = document.getElementById("rectY");
+	ry.addEventListener("input", changeRectY);
+	
+	var d = document.getElementById("depth");
+	d.addEventListener("input", changeDepth);
+	
+	var c = document.getElementById("colour");
+	c.addEventListener("change", changeColour);
+	
+	var bf = document.getElementById("boards_form");
+
+    bf.addEventListener("submit", function (event){
+		event.preventDefault()
+	});
+});
